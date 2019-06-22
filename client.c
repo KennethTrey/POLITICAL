807aa1050062712c03682e2e76b93f2aea6aef01
benon123 <benontumukunde71@gmail.com>
2019-06-22 19:13:15 +0300

    client  socket in place

---
+++ client.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>//for creating functions  like writing, reading, close 
+#include<string.h>
+#include<netdb.h>// defines the host, storing info about the host. 
+#include <netinet/in.h>
+
+
+
+int main(){
+    // create socket
+    int network_socket;
+    network_socket=socket(AF_INET,SOCK_STREAM,0);
+
+    //specify an adress for the socket
+    struct sockaddr_in server_address;
+
+     server_address.sin_family -AF_INET;
+     server_address.sin_port -htons(9002);
+     server_address.sin_addr .s_addr =INADDR_ANY;
+     
+     int connection_status =connect(network_socket,(struct sockaddr*) &server_address, sizeof(server_address)); 
+// check for the connection  error
+if(connection_status==-1){
+    printf("there was an error  in conecting to  remote  socket ");
+}
+//      receive the data from the server 
+     char server_response[256]; 
+   recv(network_socket, &server_response,sizeof(server_response),0 );
+   // print out the server's response
+   printf("the sever sent the data :%s #include",server_response );
+   system("echo used client >> use.txt");
+   
+
+
+
+
+   // then close the socket
+
+   close(network_socket);
+    return 0;
+
+}
